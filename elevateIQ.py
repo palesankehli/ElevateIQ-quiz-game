4843ffae2599b5b82d497a21d1429456aa46aa17
pankejhb025 <pnkehli@gmail.com>
2026-04-15 00:28:10 +0200

    made slight changes to the welcome banner

--- elevateIQ.py
+++ elevateIQ.py
@@ -6,12 +6,15 @@ def load_data():
         return json.load(file)
     
 BURGUNDY = '\033[38;2;128;0;32m'
+GOLD = '\033[38;2;212;175;55m'
+RESET = '\033[0m'
+BOLD = '\033[1m'
 
 def welcome_banner():
   
     print(f"\n{BURGUNDY}╔" + "═" * 80 + f"╗" )
-    print("║" + " " * 30 + "WELCOME TO ELEVATE IQ" + " " * 29 + "║")
-    print("╚" + "═" *80 + "╝")
+    print(f"║" + " " * 30 + f"{GOLD}WELCOME TO ELEVATE IQ" + " " * 29 + f"{BURGUNDY}║")
+    print(f"╚" + "═" *80 + f"╝{RESET}")
 
 welcome = welcome_banner()
 print(welcome)
