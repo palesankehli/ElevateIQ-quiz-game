d99a0a1878decfaec0773a59013e0089c7bd1d61
pankejhb025 <pnkehli@gmail.com>
2026-04-15 00:23:18 +0200

    made game welcome banner

--- elevateIQ.py
+++ elevateIQ.py
@@ -1,1 +1,17 @@
 import json
+
+
+def load_data():
+    with open('questions.json', 'r') as file:
+        return json.load(file)
+    
+BURGUNDY = '\033[38;2;128;0;32m'
+
+def welcome_banner():
+  
+    print(f"\n{BURGUNDY}╔" + "═" * 80 + f"╗" )
+    print("║" + " " * 30 + "WELCOME TO ELEVATE IQ" + " " * 29 + "║")
+    print("╚" + "═" *80 + "╝")
+
+welcome = welcome_banner()
+print(welcome)
